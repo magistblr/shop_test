ef0520d963403347ea6d7e76520df6a87834dd0d
magistblr <78726323+magistblr@users.noreply.github.com>
2022-01-10 12:28:21 +0300

    add icons

---
+++ src/main/RoutesList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+
+
+const RoutesList = () => {
+    return (
+        <Routes>
+            {/* <Route path={'/'} element={<Login/>}/>
+            <Route path='*' element={<BadRequest/>}/> */}
+        </Routes>
+    )
+}
+
+export default RoutesList;
